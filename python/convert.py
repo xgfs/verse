c5c3603776384f87ee12c78cb4b48b4732a60bd1
Anton Tsitsulin <aatsitsulin@gmail.com>
2018-10-21 15:37:04 +0200

    update converter to allow weighted graphs

--- python/convert.py
+++ python/convert.py
@@ -16,52 +16,48 @@ import numpy as np
 MAGIC = 'XGFS'.encode('utf8')
 
 
-def mat2xgfs(filename, undirected, varname):
+def mat2csr(filename, varname):
     mat = loadmat(filename)[varname].tocsr()
-    if undirected:
-        mat = mat + mat.T  # we dont care about weights in this implementation
-    return mat.indptr[:-1], mat.indices
+    return mat.indptr[:-1], mat.indices, mat.data  # we treat weights in xgfs2file
 
 
-def xgfs2file(outf, indptr, indices):
+def xgfs2file(outf, indptr, indices, weights=None):
     nv = indptr.size
     ne = indices.size
     logging.info('num vertices=%d; num edges=%d;', nv, ne)
+    assert np.all(weights > 0), 'negative weights are not allowed'
     outf.write(MAGIC)
     outf.write(pack('q', nv))
     outf.write(pack('q', ne))
     outf.write(pack('%di' % nv, *indptr))
     outf.write(pack('%di' % ne, *indices))
+    if weights is not None:
+        if not np.all(weights == 1):
+            outf.write(pack('%df' % ne, *weights))
 
 
 def is_numbers_only(nodes):
-    for node in nodes:
-        try:
-            int(node)
-        except ValueError:
-            return False
+    try:
+        list(map(int, nodes))
+    except:
+        return False
     return True
 
 
-def list2mat(input, undirected, sep):
+def list2mat(input, undirected, sep, format):
     nodes = set()
     with open(input, 'r') as inf:
         for line in inf:
-            if line.startswith('#'):
+            if line.startswith('#') or line.startswith('%'):
                 continue
             line = line.strip()
-            if sep is None:
-                splt = line.split()
-            else:
-                splt = line.split(sep)
-            if format == "edgelist":
-                if len(splt) == 3:
-                    if abs(float(splt[2]) - 1) >= 1e-4:
-                        raise ValueError("Weighted graphs are not supported")
-                    else:
-                        splt = splt[:-1]
-                else:
-                    raise ValueError("Incorrect graph format")
+            splt = line.split(sep)
+            if not splt: continue
+            if format == 'edgelist':
+                assert len(splt) == 2, 'In edgelist there should be 2 values per line '
+            if format == 'weighted_edgelist':
+                assert len(splt) == 3, 'In weighted edgelist there should be 3 values per line'
+                splt = splt[:-1]
             for node in splt:
                 nodes.add(node)
     number_of_nodes = len(nodes)
@@ -74,61 +70,59 @@ def list2mat(input, undirected, sep):
     graph = defaultdict(set)
     with open(input, 'r') as inf:
         for line in inf:
-            if line.startswith('#'):
+            if line.startswith('#') or line.startswith('%'):
                 continue
             line = line.strip()
-            if sep is None:
-                splt = line.split()
-            else:
-                splt = line.split(sep)
-            if isnumbers:
-                src = node2id[int(splt[0])]
-            else:
-                src = node2id[splt[0]]
-            if format == "edgelist" and len(splt) == 3:
+            splt = line.split(sep)
+            if not splt: continue
+            weight = 1
+            src = node2id[int(splt[0])] if isnumbers else node2id[splt[0]]
+            if format == "weighted_edgelist":
+                weight = float(splt[-1])
                 splt = splt[:-1]
             for node in splt[1:]:
                 if isnumbers:
                     tgt = node2id[int(node)]
                 else:
                     tgt = node2id[node]
-                graph[src].add(tgt)
+                graph[src].add((tgt, weight))
                 if undirected:
-                    graph[tgt].add(src)
+                    graph[tgt].add((src, weight))
     indptr = np.zeros(number_of_nodes + 1, dtype=np.int32)
     indptr[0] = 0
     for i in range(number_of_nodes):
         indptr[i + 1] = indptr[i] + len(graph[i])
     number_of_edges = indptr[-1]
     indices = np.zeros(number_of_edges, dtype=np.int32)
+    weights = np.zeros(number_of_edges, dtype=np.int32)
     cur = 0
     for node in range(number_of_nodes):
         for adjv in sorted(graph[node]):
-            indices[cur] = adjv
+            indices[cur], weights[cur] = adjv
             cur += 1
-    return indptr[:-1], indices
+    return indptr[:-1], indices, weights
 
 
 def process(format, matfile_variable_name, undirected, sep, input, output):
     if format == "mat":
-        indptr, indices = mat2xgfs(input, undirected, matfile_variable_name)
-    elif format in ['edgelist', 'adjlist']:
-        indptr, indices = list2mat(input, undirected, sep)
+        indptr, indices, weights = mat2csr(input, matfile_variable_name)
+    elif format in ['weighted_edgelist', 'edgelist', 'adjlist']:
+        indptr, indices, weights = list2mat(input, undirected, sep, format)
 
     with open(output, 'wb') as fout:
-        xgfs2file(fout, indptr, indices)
+        xgfs2file(fout, indptr, indices, weights)
 
 
 @click.command(help=__doc__)
 @click.option('--format',
               default='edgelist',
-              type=click.Choice(['mat', 'edgelist', 'adjlist']),
+              type=click.Choice(['mat', 'weighted_edgelist', 'edgelist', 'adjlist']),
               help='File format of input file')
 @click.option('--matfile-variable-name', default='network',
               help='variable name of adjacency matrix inside a .mat file.')
-@click.option('--undirected/--directed', default=True, is_flag=True,
-              help='Treat graph as undirected.')
-@click.option('--sep', default=' ', help='Separator of input file')
+@click.option('--undirected/--directed', default=False, is_flag=True,
+              help='Treat graph as undirected. Affects adjlist or edgelist file formats.')
+@click.option('--sep', default=None, help='Separator of input file. Only affects adjlist and edgelists.')
 @click.argument('input', type=click.Path())
 @click.argument('output', type=click.Path())
 def main(format, matfile_variable_name, undirected, sep, input, output):
